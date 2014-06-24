2a5edba89c52bf67a9913e8e7b7bc1e55d3ff189
Jonny Andrews <jonny.andrews@GL04325M.local>
2014-06-24 18:56:42 +0200

    experiment with wait

--- onMinute.sh
+++ onMinute.sh
@@ -11,6 +11,7 @@ doActions(){
 	## Put actions here
 
 	playMedia "http://media2.giphy.com/media/XNzYBOhw7C3mw/giphy.gif"
+	wait 5
 	playMedia "http://media.giphy.com/media/x5aebCMe2PUCA/giphy.gif"
 	
 
