26cbe813a182aea49c270ba1a81bc320c5247e52
jonnyAnd <jonny@eino.co.uk>
2014-06-25 11:50:54 +0200

    Adding lots of media to test cacheing

--- core/onMinute.sh
+++ core/onMinute.sh
@@ -21,6 +21,17 @@ doActions(){
 	## Put actions here
 
 	playMedia "http://i.imgur.com/dAtcCfH.gif"
+	
+	playMedia "http://i.imgur.com/VmGaKOG.gif";
+	playMedia "http://i.imgur.com/NOJMaGg.gif";
+	playMedia "http://i.imgur.com/nBTu3oi.gif";
+	playMedia "http://i.imgur.com/vzokKQ9.gif";
+	##playMedia "http://i.imgur.com/2CCIIMk.gif";
+	
+	
+	
+	
+	
 
 	##playMedia "http://media2.giphy.com/media/XNzYBOhw7C3mw/giphy.gif"
 	##wait 5
