67f3620c4d00361d512ef4b2212125debd35a64d
jonnyAnd <jonny@eino.co.uk>
2014-07-03 12:48:35 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,7 @@
 
 testme(){
 
-	sudo curl https://yt-dl.org/downloads/2014.06.26/youtube-dl -o /usr/local/bin/youtube-dl
-
-	sudo chmod a+x /usr/local/bin/youtube-dl
-
+	youtube-dl "http://www.youtube.com/watch?v="$1
 }
 
-testme
+testme "moZ4qoFIeUE"
