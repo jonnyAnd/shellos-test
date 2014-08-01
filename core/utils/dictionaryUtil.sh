674d546817434ca411d717ba5e453f1fca6c06a1
Jonny Andrews <jonny.andrews@GL04325M.local>
2014-08-01 14:54:12 +0200

    testing random you tube stuff

--- core/utils/dictionaryUtil.sh
+++ core/utils/dictionaryUtil.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 
-getRandomWordFromFile(){
+dictionaryUtil_getRandomWordFromFile(){
 
 	SOURCE_FILE=$1
 
 
-	cat $SOURCE_FILE| sort --random-sort | head -n 1
+	##head -$((${RANDOM} % `wc -l < $SOURCE_FILE` + 1)) $SOURCE_FILE | tail -1
 
+	echo $(head -n 1 $SOURCE_FILE)
 
+	
+
+	##echo $RANDOM
 
 
 }
